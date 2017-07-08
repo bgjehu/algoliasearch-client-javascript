e9376a7355611331a443e8d89f12f284a64cc6e8
jay <jay@insiderx.com>
2017-07-07 23:10:07 -0500

    reproduce problem

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-'use strict';
+const algolia = require('algoliasearch');
+const {isEmpty, remove} = require('lodash');
 
-// default entrypoint is the node module
-// this is overriden by the `browser` field in package.json
-// https://github.com/substack/node-browserify#browser-field
-module.exports = require('./src/server/builds/node');
+const json = require('./bs.json');
+
+const client = algolia('applicationID', 'apiKey');
+const index = client.initIndex('index');
+
+const push = obj => new Promise((resolve, reject) => index.addObjects(obj, (err, content) => (err ? reject : resolve)(err ? err : content)));
+
+(async arr => {
+    const cap = 4000;
+    let pushed = 0;
+    const all = arr.length;
+    while (!isEmpty(arr)) {
+        const cur = remove(arr, (obj, index) => index < cap);
+        const {length} = cur;
+        await push(cur);
+        pushed += length;
+        console.log(`Pushed ${length} entries (all: ${all}, pushed: ${pushed}, left: ${all - pushed})`);
+    }
+})(json).catch(err=>console.log(err.message));
